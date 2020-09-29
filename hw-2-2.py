9b3cc6db7b0b3695c7adbd41a85f68ce057879c1
christian lalanne <p21clalanne@fairfieldprep.org>
2020-09-29 12:36:09 -0400

    hw 2-2 submitted correctly

---
+++ hw-2-2.py
@@ -0,0 +1,18 @@
+#Author CL 9//22
+
+f = 1
+i = 2
+o = 3
+
+print(f)
+print(i)
+print(o)
+
+TS = (f)+(i)+(o)
+print(TS)
+
+player = (6*(f)+8*(i)+4*(o))
+
+print(player)
+
+print("the player scored 34 points in the game")
